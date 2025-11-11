9951cea6718b0c93ffeec56ea7e71271d34d0057
Moe <mohammed.kaldi@outlook.com>
2025-11-11 21:12:50 +0100

    Skelet för Morseöversättare

---
+++ src/Morseöversättare.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Morseöversättare {
+
+    private Map<String, String> engTillMorse = new HashMap<>();
+    private Map<String, String> morseTillEng = new HashMap<>();
+
+    public Morseöversättare() {
+
+    }
+
+    public String engelskatillMorse(String text) {
+
+        return null;
+    }
+
+    public String morstillEngelska(String morseKod) {
+
+        return null;
+    }
+}
